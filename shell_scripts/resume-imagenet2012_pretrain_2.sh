8397a05f7b234fa5e0ede347d9061527b901dc68
Zhongxiao Li <liz0f@login510-27.ibex.kaust.edu.sa>
2021-07-03 21:11:32 +0300

    changed lr

--- shell_scripts/resume-imagenet2012_pretrain_2.sh
+++ shell_scripts/resume-imagenet2012_pretrain_2.sh
@@ -6,12 +6,12 @@ python -m vit_jax.train \
     --vit_pretrained_dir pretrained_vit_models \
     --dataset imagenet2012 \
     --batch 1280 \
-    --base_lr 1e-3 \
+    --base_lr 4.262e-4 \
     --decay_type cosine \
     --linear_end 1e-5 \
     --warmup_steps 500 \
     --eval_every 1000 \
-    --total_steps 80000 \
+    --total_steps 10000000 \
     --weight_decay 0.00 \
     --resume_dir train_dir/imagenet2012_pretrain_2/2021-06-13-13:00:27 \
     --resume_from step-44000.npz
